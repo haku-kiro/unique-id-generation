507c4cc7d667c02b9c6c1db442acfb39d14e7d6f
mdjcosta30 <mdjcosta30@gmail.com>
2024-10-17 20:16:01 +0200

    impl of using a http client and server to generate the numbers

--- main.go
+++ main.go
@@ -1,27 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"log"
-	"math/rand"
+	"net/http"
+	"strconv"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-var (
-	filePath = "./id"
-)
+func getId() (int, error) {
+	resp, err := http.Get("http://localhost:8090/serve-id")
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	// The id server should return just a single number, no other content.
+	scanner := bufio.NewScanner(resp.Body)
+	scanner.Scan()
+	data := scanner.Text()
+	return strconv.Atoi(data)
+}
 
 func main() {
 	n := maelstrom.NewNode()
 	n.Handle("generate", func(msg maelstrom.Message) error {
+		id, err := getId()
+		if err != nil {
+			return err
+		}
+
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
 		body["type"] = "generate_ok"
-		body["id"] = rand.Intn(1000_000_000)
+		body["id"] = id
 
 		return n.Reply(msg, body)
 	})
