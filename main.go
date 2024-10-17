efeb689d88dd2169f678ab7175178d7c949b99b1
mdjcosta30 <mdjcosta30@gmail.com>
2024-10-17 19:51:00 +0200

    Just generating a random integer

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
-	"os"
-	"strconv"
+	"math/rand"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -14,48 +12,16 @@ var (
 	filePath = "./id"
 )
 
-func storeId(id int) error {
-	stringId := fmt.Sprint(id)
-	// Assuming we're overwriting the file each time?
-	return os.WriteFile(filePath, []byte(stringId), 0644)
-}
-
-func readId() (int, error) {
-	data, err := os.ReadFile(filePath)
-	if err != nil {
-		return 0, err
-	}
-
-	return strconv.Atoi(string(data))
-}
-
 func main() {
-	// Seeding our id
-	err := storeId(0)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	n := maelstrom.NewNode()
 	n.Handle("generate", func(msg maelstrom.Message) error {
-		id, err := readId()
-		if err != nil {
-			return err
-		}
-		id += 1
-
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
 		body["type"] = "generate_ok"
-		body["id"] = id
-
-		err = storeId(id)
-		if err != nil {
-			return err
-		}
+		body["id"] = rand.Intn(1000_000_000)
 
 		return n.Reply(msg, body)
 	})
